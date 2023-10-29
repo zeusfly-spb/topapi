10cf4643d9ee11d1e5df751e315f5376fcc28550
Олег Сайфутдинов <zeusfly@mail.ru>
2023-10-30 00:34:06 +0300

    Updating layouts

---
+++ topapi-front/layouts/default.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <v-app>
+      <v-app-bar>
+        <span
+          class="ml-5 unselectable"
+          style="cursor: pointer"
+          title="На главную"
+          @click="navigateTo('/')"
+        >
+          TopApi
+        </span>
+        <v-spacer />
+<!--        <MainMenu v-if="authenticated" />-->
+        <template >
+          <v-btn @click="authDialog = true"> Войти </v-btn>
+        </template>
+      </v-app-bar>
+      <v-main class="mt-2">
+        <slot />
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<style scoped></style>
